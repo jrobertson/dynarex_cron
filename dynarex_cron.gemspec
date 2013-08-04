3cf648396f37cacdebd27487d4afeabc6a26a15b
James Robertson <james@r0bertson.co.uk>
2013-08-04 23:06:55 +0100

    bug fix: moved dynarex-events class into dynarex_cron.rb to avoid circular dependencies

--- dynarex_cron.gemspec
+++ dynarex_cron.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'dynarex_cron'
-  s.version = '0.2.0'
+  s.version = '0.2.1'
   s.summary = 'dynarex_cron'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
@@ -8,7 +8,7 @@ Gem::Specification.new do |s|
   s.add_dependency('chronic_cron')
   s.add_dependency('simplepubsub')  
   s.add_dependency('rscript')
-  s.add_dependency('dynarex-events')
+  s.add_dependency('chronic_duration')
   s.signing_key = '../privatekeys/dynarex_cron.pem'
   s.cert_chain  = ['gem-public_cert.pem']  
   s.license = 'MIT'
--- lib/dynarex_cron.rb
+++ lib/dynarex_cron.rb
@@ -6,7 +6,7 @@ require 'dynarex'
 require 'chronic_cron'
 require 'simplepubsub'
 require 'rscript'
-require 'dynarex-events'
+require 'chronic_duration'
 
 DF = "%Y-%m-%d %H:%M"
 
@@ -114,4 +114,71 @@ class DynarexCron
 
   end
 
+end
+
+class DynarexEvents < DynarexCron
+
+  attr_reader :to_a
+
+  def initialize(dynarex_file=nil, options={})
+    
+    opt = {sps_address: nil, drb_server: false}.merge options
+    
+    @cron_events  = []
+    
+    @dynarex_file = dynarex_file
+    load_events()
+
+    @sps_address = opt[:sps_address]
+    
+    if opt[:drb_server] == true then
+      
+      Thread.new {
+        
+        # start up the DRb service
+        DRb.start_service 'druby://:57500', self
+
+        # wait for the DRb service to finish before exiting
+        DRb.thread.join    
+      }
+    end
+  end    
+
+  def load_events()
+    
+    dynarex = Dynarex.new @dynarex_file
+    @entries = dynarex.to_h
+    @cron_events = self.to_a
+  end  
+  
+  alias refresh load_events
+  
+  def start
+    @running = true
+    puts '[' + Time.now.strftime(DF) + '] DynarexEvents started'
+    
+    while @running == true
+      iterate @cron_events
+      sleep 60 # wait for 60 seconds
+    end
+  end  
+  
+  def to_a()
+    
+    @entries.inject([]) do |r,h| 
+
+      h[:cron] = ChronicCron.new(h[:date]) 
+      h[:job] = 'pub event: ' + h[:title]
+
+      if h[:reminder].length > 0 then
+        rmndr = {}
+        rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - ChronicDuration.parse(h[:reminder])).to_s)
+        rmndr[:job] = 'pub event: reminder ' + h[:title]
+        r << rmndr
+      end
+
+      r << h
+    end
+    
+  end
 end
