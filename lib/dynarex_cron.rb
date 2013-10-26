c0a34cc92329d852c7d3fc0adfdcc5f8e327ac88
James Robertson <james@r0bertson.co.uk>
2013-10-26 22:25:14 +0100

    dynarex-events modified for optional dynarex file. Dynarex_cron drb_server now expecting an optional port number

--- lib/dynarex_cron.rb
+++ lib/dynarex_cron.rb
@@ -13,9 +13,11 @@ DF = "%Y-%m-%d %H:%M"
 
 class DynarexCron
 
+  # options: e.g. sps_address, 'sps', drb_server: 57000
+  #
   def initialize(dynarex_file=nil, options={})
     
-    opt = {sps_address: nil, drb_server: false, log: nil}.merge options
+    opt = {sps_address: nil, drb_server: nil, log: nil}.merge options
     @logger = Logger.new(opt[:log],'weekly') if opt[:log]
     
     @cron_entries, @cron_events  = [], []
@@ -26,12 +28,12 @@ class DynarexCron
 
     @sps_address = opt[:sps_address]
     
-    if opt[:drb_server] == true then
+    if opt[:drb_server] then
       
       Thread.new {
         
         # start up the DRb service
-        DRb.start_service 'druby://:57000', self
+        DRb.start_service 'druby://:' + opt[:drb_server], self
 
         # wait for the DRb service to finish before exiting
         DRb.thread.join    
@@ -124,27 +126,30 @@ class DynarexCron
 
 end
 
+
 class DynarexEvents < DynarexCron
 
   attr_reader :to_a
 
+  # options: e.g. sps_address, 'sps', drb_server: 58000
+  #
   def initialize(dynarex_file=nil, options={})
     
-    opt = {sps_address: nil, drb_server: false}.merge options
+    opt = {sps_address: nil, drb_server: nil}.merge options
     
-    @cron_events  = []
-    
+    @entries, @cron_events  = [], []
+
     @dynarex_file = dynarex_file
     load_events()
 
     @sps_address = opt[:sps_address]
     
-    if opt[:drb_server] == true then
-      
+    if opt[:drb_server] then
+
       Thread.new {
         
         # start up the DRb service
-        DRb.start_service 'druby://:57500', self
+        DRb.start_service 'druby://:' + opt[:drb_server], self
 
         # wait for the DRb service to finish before exiting
         DRb.thread.join    
@@ -152,11 +157,18 @@ class DynarexEvents < DynarexCron
     end
   end    
 
+  def add_entry(h)
+    # if the entry already exists delete it
+    @entries.delete @entries.find {|x| x[:job] == h[:job]}
+    @entries << h
+  end
+
   def load_events()
     
     dynarex = Dynarex.new @dynarex_file
-    @entries = dynarex.to_h
-    @cron_events = self.to_a
+    @entries = dynarex.to_h || @entries
+    @cron_events = self.to_a if @entries
+    'events loaded and refreshed'
   end  
   
   alias refresh load_events
@@ -172,16 +184,17 @@ class DynarexEvents < DynarexCron
   end  
   
   def to_a()
-    
+
+    return unless @entries
+
     @entries.inject([]) do |r,h| 
 
-      h[:cron] = ChronicCron.new(h[:date]) 
-      h[:job] = 'pub event: ' + h[:title]
+      h[:cron] = ChronicCron.new(h[:date] + ' ' + h[:recurring].to_s ) 
+      h[:job] ||= 'pub event: ' + h[:title]
 
-      if h[:reminder].length > 0 then
+      if h[:reminder].to_s.length > 0 then
         rmndr = {}
-        rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - 
-                                     ChronicDuration.parse(h[:reminder])).to_s)
+        rmndr[:cron] = ChronicCron.new((Chronic.parse(h[:date]) - ChronicDuration.parse(h[:reminder])).to_s)
         rmndr[:job] = 'pub event: reminder ' + h[:title]
         r << rmndr
       end
