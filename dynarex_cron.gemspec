e21dd383c78c1a86ee4fa8c351e6551350763e43
James Robertson <james@r0bertson.co.uk>
2022-02-23 19:18:17 +0000

    bug fix: Replaced  RXFHelper with RXFReader.

--- dynarex_cron.gemspec
+++ dynarex_cron.gemspec
@@ -1,17 +1,17 @@
 Gem::Specification.new do |s|
   s.name = 'dynarex_cron'
-  s.version = '0.9.4'
+  s.version = '0.9.5'
   s.summary = 'Publishes SimplePubSub messages by reading cron ' + 
       'entries from a Dynarex document'
   s.authors = ['James Robertson']
   s.files = Dir['lib/dynarex_cron.rb']
-  s.add_runtime_dependency('chronic_cron', '~> 0.3', '>=0.3.7')
-  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.27')
+  s.add_runtime_dependency('chronic_cron', '~> 0.7', '>=0.7.1')
+  s.add_runtime_dependency('dynarex', '~> 1.9', '>=1.9.6')
   s.add_runtime_dependency('sps-pub', '~> 0.5', '>=0.5.5')  
   s.signing_key = '../privatekeys/dynarex_cron.pem'
   s.cert_chain  = ['gem-public_cert.pem']  
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/dynarex_cron'
   s.required_ruby_version = '>= 2.1.2'
 end
--- lib/dynarex_cron.rb
+++ lib/dynarex_cron.rb
@@ -5,47 +5,49 @@
 require 'dynarex'
 require 'chronic_cron'
 require 'sps-pub'
+require 'rxfreader'
 
 
 DF = "%Y-%m-%d %H:%M"
 
 class DynarexCron
+  using ColouredText
 
   # options: e.g. sps_address: 'sps', sps_port: '59000'
   #
   def initialize(dxfile=nil, sps_address: 'sps', sps_port: '59000',  \
-      log: nil, time_offset: 0, logtopic: 'DynarexCron')
-    
+      log: nil, time_offset: 0, logtopic: 'DynarexCron', debug: false)
+
 
     @dxfile, @sps_address, @sps_port, @log = dxfile, sps_address, sps_port, log
 
-    # time_offset: used for testing a cron entry without having to change 
+    # time_offset: used for testing a cron entry without having to change
     #              the time of each entry
     @time_offset = time_offset.to_i
-    
+
     @cron_entries = []
     @logtopic = logtopic
-    
+
 
     if @dxfile then
 
       dynarex,_ = load_doc dxfile
       load_entries(dynarex)
-    end 
+    end
 
     @pub = SPSPub.new address: sps_address, port: sps_port
-    
+
   end
 
   def start
 
     @running = true
-    
+
     if @log then
       @log.info @logtopic + '/start: Time.now: ' \
           + (Time.now + @time_offset).strftime(DF)
     end
-        
+
 
     sleep 1 until Time.now.sec == 0
 
@@ -56,17 +58,17 @@ class DynarexCron
 
       if @dxfile.is_a? String then
 
-        # What happens if the @dxfile is a URL and the web server is 
+        # What happens if the @dxfile is a URL and the web server is
         # temporarily unavailable? i.e. 503 Service Temporarily Unavailable
         begin
 
           dx, buffer = load_doc(@dxfile)
-          load_entries dx if @buffer != buffer          
+          load_entries dx if @buffer != buffer
           @buffer = buffer
-          
+
         rescue
-          @log.debug @logtopic + '/start: warning: ' + ($!).inspect
-        end        
+          @log.debug @logtopic + '/start: warning: ' + ($!).inspect if @log
+        end
 
       end
 
@@ -76,11 +78,11 @@ class DynarexCron
     end
 
   end
-  
+
   def stop()
     @running = false
   end
-  
+
   private
 
   def load_doc(dynarex_file)
@@ -88,42 +90,43 @@ class DynarexCron
     if dynarex_file.is_a?(Dynarex) then
       dynarex_file
     else
-      
-      buffer, _ = RXFHelper.read(dynarex_file)      
+
+      buffer, _ = RXFReader.read(dynarex_file)
       dx = buffer =~ /<\?dynarex/ ? Dynarex.new.import(buffer) : \
           Dynarex.new(buffer)
-      
+
       [dx, buffer]
     end
-        
+
   end
 
   def load_entries(dx)
-    
+
     if dx.summary[:sps_address] then
       @sps_address, @sps_port = dx.summary[:sps_address]\
                                                     .split(':',2) << '59000'
     end
-    
+
     @cron_entries = dx.to_a
-    @cron_entries.each do |h| 
+    @cron_entries.each do |h|
       h[:cron] = ChronicCron.new(h[:expression], Time.now + @time_offset)
     end
-  end  
-  
+  end
+
 
   def iterate(cron_entries)
-    
+
+    puts ('h:' +  h.inspect).debug if @debug
     cron_entries.each do |h|
-      
+
       datetime = (Time.now + @time_offset).strftime(DF)
-      
+
       if @log then
         @log.info @logtopic + '/iterate: datetime: ' + datetime
         @log.info @logtopic + '/iterate: cron.to_time: ' \
             + h[:cron].to_time.strftime(DF)
       end
-      
+
       if h[:cron].to_time.strftime(DF) == datetime then
 
         begin
@@ -131,23 +134,23 @@ class DynarexCron
           if h[:fqm].empty? and @log then
             @log.debug @logtopic + '/iterate: no h[:fqw] found ' + h.inspect
           end
-          
+
           msg = h[:fqm].gsub('!Time',Time.now.strftime("%H:%M"))
 
           @pub.notice msg
 
         rescue
-          
+
           if @log then
             @log.debug @logtopic + '/iterate: cron: ' + h[:cron].inspect
             @log.debug @logtopic + '/iterate: h: ' + h.inspect + ' : ' \
                 + ($!).inspect
           end
-          
+
         end
-        
+
         h[:cron].next # advances the time
-            
+
       end
 
     end
